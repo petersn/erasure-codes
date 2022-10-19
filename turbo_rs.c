5435ef913fc9377a9fc74e4614cf06959a0772ed
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2022-10-19 16:45:25 -0700

    Working JIT.

--- turbo_rs.c
+++ turbo_rs.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <assert.h>
+#include <stdio.h>
 
 #if defined(_M_X64) && !defined(__x86_64__)
 #define __x86_64__
@@ -108,8 +109,8 @@ typedef struct reed_solomon_t {
     int input_count;
     int output_count;
     uint32_t *table;
-    uint32_t *table2;
-    void *jit_code;
+    uint32_t *bytewise_table;
+    uint8_t *jit_code;
     size_t jit_code_length;
 } reed_solomon_t;
 
@@ -158,8 +159,8 @@ void reed_solomon_init(
     ctx->table = (uint32_t*) malloc(
         sizeof(uint32_t) * input_count * output_count * degree
     );
-    ctx->table2 = (uint32_t*) malloc(
-        sizeof(uint32_t) * input_count * output_count * degree
+    ctx->bytewise_table = (uint32_t*) malloc(
+        sizeof(uint32_t) * input_count * output_count
     );
     ctx->jit_code = NULL;
     ctx->jit_code_length = 0;
@@ -168,6 +169,8 @@ void reed_solomon_init(
 void reed_solomon_free(reed_solomon_t *ctx) {
     free(ctx->table);
     ctx->table = NULL;
+    free(ctx->bytewise_table);
+    ctx->bytewise_table = NULL;
     if (ctx->jit_code != NULL)
         munmap(ctx->jit_code, ctx->jit_code_length);
     ctx->jit_code = NULL;
@@ -193,36 +196,173 @@ void reed_solomon_configure(
                     field_inv(ctx->polynomial, input_x[i] ^ input_x[p])
                 ));
             }
-            ctx->table2[i * ctx->output_count + j] = accum;
+            ctx->bytewise_table[i * ctx->output_count + j] = accum;
             for (int input_bit = 0; input_bit < degree; input_bit++) {
                 ctx->table[i * ctx->output_count * degree + j * degree + input_bit] =
                     field_mul(ctx->polynomial, accum, 1ull << input_bit);
             }
         }
     }
+
+#ifndef NO_JIT
     // JIT compile the code.
     if (ctx->jit_code != NULL)
         munmap(ctx->jit_code, ctx->jit_code_length);
     ctx->jit_code = mmap(
         NULL,
-        4096,
+        4096 * 16,
         PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS,
         -1,
         0
     );
+
+#ifdef __x86_64__
+    // Check if we have AVX2.
+    if (__builtin_cpu_supports("avx2")) {
+        jit_compile_avx2(ctx);
+    } else {
+        jit_compile_sse2(ctx);
+    }
+#endif
+
+#ifdef __aarch64__
+    if (__builtin_cpu_supports("neon")) {
+        jit_compile_neon(ctx);
+    } else {
+        jit_compile_unvectorized_arm64(ctx);
+    }
+#endif
+
+#endif
 }
 
-void reed_solomon_process_jit(
-) {
-    
-}
-
-void reed_solomon_process_no_jit(
+uint64_t call_jit(
     reed_solomon_t *ctx,
     const char * restrict * input_shard_data,
     char * restrict * output_shard_data,
     size_t shard_length
 ) {
+    uint64_t (*jit_func)(
+        const char * restrict * input_shard_data,
+        char * restrict * output_shard_data,
+        size_t shard_length
+    ) = (uint64_t (*)(const char * restrict *, char * restrict *, size_t))ctx->jit_code;
+    int degree = get_degree(ctx->polynomial);
+    shard_length /= 32 * degree;
+    return jit_func(input_shard_data, output_shard_data, shard_length);
+}
+
+void jit_compile_avx2(reed_solomon_t *ctx) {
+    // Our function takes:
+    //   rdi: const __m256i ** input_shard_data
+    //   rsi: __m256i ** output_shard_data
+    //   rdx: uint64_t block_count
+    int degree = get_degree(ctx->polynomial);
+    assert(degree <= 15);
+    int stride = 32 * degree;
+    //int storage_bytes = 0;
+    FILE* f = fopen("jit_avx2.s", "w");
+    fprintf(f,
+        "bits 64\n"
+        //"  push rbp\n"
+        //"  mov rbp, rsp\n"
+        //"  sub rsp, %i\n"
+        "  xor rcx, rcx\n" // rcx is our loop counter.
+        "  xor rax, rax\n"
+        "  mov r8, rdx\n"
+        "  test rdx, rdx\n"
+        "  je .end\n"
+        ".loop:\n"
+        //storage_bytes
+    );
+    int ymm_has_value[16] = {0};
+    // Loop over each output.
+    for (int j = 0; j < ctx->output_count; j++) {
+        fprintf(f, "  ; Output %i\n", j);
+        // Zero out our scratch.
+        for (int i = 0; i < 16; i++)
+            ymm_has_value[i] = 0;
+        // Loop over each input.
+        for (int i = 0; i < ctx->input_count; i++) {
+            // Compute the input pointer as r9.
+            fprintf(f,
+                "  ; r9 = input_shard_data[%i] + offset\n"
+                "  mov r9, [rdi + %i * 8]\n"
+                "  add r9, rax\n",
+                i, i
+            );
+            // Process a block of inputs.
+            for (int input_bit = 0; input_bit < degree; input_bit++) {
+                fprintf(f, "  vmovdqu ymm15, [r9 + %i]\n", 32 * input_bit);
+                uint32_t mask = ctx->table[i * ctx->output_count * degree + j * degree + input_bit];
+                for (int k = 0; k < degree; k++) {
+                    if (!((mask >> k) & 1))
+                        continue;
+                    if (ymm_has_value[k]) {
+                        fprintf(f, "  vpxor ymm%i, ymm%i, ymm15\n", k, k);
+                    } else {
+                        fprintf(f, "  vmovdqu ymm%i, ymm15\n", k);
+                        ymm_has_value[k] = 1;
+                    }
+                }
+            }
+        }
+        // Write outputs.
+        fprintf(f,
+            "  ; r10 = output_shard_data[%i] + offset\n"
+            "  mov r10, [rsi + %i * 8]\n"
+            "  add r10, rax\n",
+            j, j
+        );
+        for (int b = 0; b < degree; b++) {
+            assert(ymm_has_value[b]);
+            fprintf(f, "  vmovdqu [r10 + %i], ymm%i\n", 32 * b, b);
+        }
+    }
+    fprintf(f,
+        "  add rcx, 1\n"
+        "  add rax, %i\n"
+        "  cmp rcx, r8\n"
+        "  jne .loop\n"
+        ".end:\n"
+        //"  leave\n"
+        "  ret\n",
+        32 * degree
+    );
+    fclose(f);
+    // Assemble the code.
+    system("nasm -f bin -o jit_avx2.bin jit_avx2.s");
+    // Load the code.
+    f = fopen("jit_avx2.bin", "rb");
+    if (f == NULL) {
+        fprintf(stderr, "Failed to open jit_avx2.bin\n");
+        exit(1);
+    }
+    fseek(f, 0, SEEK_END);
+    size_t code_length = ftell(f);
+    printf("code length: %zu\n", code_length);
+    fseek(f, 0, SEEK_SET);
+    fread(ctx->jit_code, 1, code_length, f);
+    fclose(f);
+    // Print the code in hex.
+    for (int i = 0; i < code_length; i++) {
+        printf("%02x", ((uint8_t*) ctx->jit_code)[i]);
+    }
+    printf("\n");
+
+    printf("JITted\n");
+}
+
+void jit_compile_sse2(reed_solomon_t *ctx) {
+    printf("Not implemented\n");
+}
+
+void jit_compile_neon(reed_solomon_t *ctx) {
+    
+}
+
+void jit_compile_unvectorized_arm64(reed_solomon_t *ctx) {
+    
 }
 
 void reed_solomon_process_no_jit_bytewise(
@@ -238,7 +378,7 @@ void reed_solomon_process_no_jit_bytewise(
     }
     for (int i = 0; i < ctx->input_count; i++) {
         for (int j = 0; j < ctx->output_count; j++) {
-            uint8_t coeff = ctx->table2[i * ctx->output_count + j];
+            uint8_t coeff = ctx->bytewise_table[i * ctx->output_count + j];
             const uint8_t *input_ptr = input_shard_data[i];
             uint8_t *output_ptr = output_shard_data[j];
             for (size_t k = 0; k < shard_length; k++)
@@ -268,8 +408,8 @@ void reed_solomon_process_no_jit_avx2(
     __m256i ** output_vec = (__m256i **) output_shard_data;
     uint32_t *table = ctx->table;
     __m256i scratch[degree];
-    for (int j = 0; j < output_count; j++) {
-        for (size_t segment = 0; segment < segments; segment++) {
+    for (size_t segment = 0; segment < segments; segment++) {
+        for (int j = 0; j < output_count; j++) {
             __m256i * restrict output_ptr = output_vec[j] + segment * degree;
             for (int b = 0; b < degree; b++)
                 scratch[b] = _mm256_setzero_si256();
@@ -299,8 +439,6 @@ void* aligned_malloc(size_t size, size_t alignment) {
     return ptr;
 }
 
-#include <stdio.h>
-
 //#define BYTES (1024 * 1024)
 #define BYTES 96
 
@@ -308,15 +446,16 @@ int main() {
     // Example usage.
     reed_solomon_t ctx;
     reed_solomon_init(&ctx, 0b1011/*0x11d*/, 5, 3);
+
     //reed_solomon_init(&ctx, 0x11b, 5, 3);
     int input_x[5] = {0, 1, 2, 3, 4};
     int output_x[3] = {5, 6, 7};
     reed_solomon_configure(&ctx, input_x, output_x);
 
-    // Print table2.
+    // Print bytewise_table.
     for (int i = 0; i < ctx.input_count; i++) {
         for (int j = 0; j < ctx.output_count; j++) {
-            printf("%02x ", ctx.table2[i * ctx.output_count + j]);
+            printf("%02x ", ctx.bytewise_table[i * ctx.output_count + j]);
         }
         printf("\n");
     }
@@ -329,9 +468,12 @@ int main() {
             shards[i][j] = i < 5 ? i + j : 0;
         }
     }
-    reed_solomon_process_no_jit_avx2(
-        &ctx, (const char**)shards, shards + 5, BYTES
-    );
+    uint64_t result = call_jit(&ctx, shards, shards + 5, BYTES);
+    printf("Result: %lu\n", result);
+
+    //reed_solomon_process_no_jit_avx2(
+    //    &ctx, (const char**)shards, shards + 5, BYTES
+    //);
     // Print out the first few bytes of the shards.
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 16; j++) {
@@ -339,16 +481,15 @@ int main() {
         }
         printf("\n");
     }
-    //return 0;
 
     // Reconfigure to recover.
     int input_x2[5] = {0, 2, 4, 5, 7};
     int output_x2[3] = {1, 3, 6};
     reed_solomon_configure(&ctx, input_x2, output_x2);
-    // Print table2.
+    // Print bytewise_table.
     for (int i = 0; i < ctx.input_count; i++) {
         for (int j = 0; j < ctx.output_count; j++) {
-            printf("%02x ", ctx.table2[i * ctx.output_count + j]);
+            printf("%02x ", ctx.bytewise_table[i * ctx.output_count + j]);
         }
         printf("\n");
     }
@@ -362,9 +503,11 @@ int main() {
         memset(shards2[i], 0, BYTES);
     }
 
-    reed_solomon_process_no_jit_avx2(
-        &ctx, (const char**)shards2, shards2 + 5, BYTES
-    );
+    //reed_solomon_process_no_jit_avx2(
+    //    &ctx, (const char**)shards2, shards2 + 5, BYTES
+    //);
+    result = call_jit(&ctx, (const char**)shards2, shards2 + 5, BYTES);
+    printf("Result: %lu\n", result);
     // Compare the recovered shards.
     int bad = 0;
     for (int i = 0; i < 3; i++) {
@@ -381,7 +524,4 @@ int main() {
         printf("\n======\n");
     }
     printf("BAD: %d\n", bad);
-
-    //reed_solomon_process_jit(&ctx, RS_PROCESSING_MODE_AVX2);
-
 }
